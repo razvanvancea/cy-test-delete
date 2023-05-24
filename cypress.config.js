d849b1c0df8ed7a330201d41b0abd13a258ee473
Razvan Vancea <razvan.vancea@zitec.com>
2023-05-24 13:36:26 +0300

    fix

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 15000,
+  pageLoadTimeout: 15000,
+  retries: 1,
   video: false,
   e2e: {
     setupNodeEvents(on, config) {
